e94aa668ce9593dcd3532f1067d06c6925411445
Patrick <Patrick@zolder>
2013-09-05 22:34:31 +0200

    Revert "Revert "removed debug""
    This reverts commit 99fdec89daa2a204dd279ed0cf47191279e61653.

--- IntSet/test/intset/IntSetTest.java
+++ IntSet/test/intset/IntSetTest.java
@@ -1,276 +1,235 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package intset;
 
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
+import org.junit.Before;
 
 /**
  *
- * @author Rik Schaaf
+ * @author Patrick Beuks (s2288842) & Rik Schaaf
  */
 public class IntSetTest {
-    private IntSet instance;
-    private static final int TEST_CAPACITY = 7;
-    private static final int TEST_VALUE = 243;
 
-    public IntSetTest() {
-    }
-
-    @BeforeClass
-    public static void setUpClass() {
-        
-    }
-
-    @AfterClass
-    public static void tearDownClass() {
-    }
+    private IntSet intSet;
+    private static final int CAPASITY = 10;
 
     @Before
     public void setUp() {
-        instance = new IntSet(TEST_CAPACITY);
+        intSet = new IntSet(CAPASITY);
     }
 
-    @After
-    public void tearDown() {
-    }
-
-    /**
-     * Test of isEmpty method, of class IntSet.
-     */
-    
-    @Test
-    public void testIntSet() {
-        System.out.println("constructor");
-        instance = null;
-        assert TEST_CAPACITY >= 0;
-        instance = new IntSet(TEST_CAPACITY);
-        assertEquals(0, instance.getCount());
-        assertEquals(TEST_CAPACITY, instance.getCapacity());
-    }
-    
-    @Test
-    public void testIsEmpty() {
-//        System.out.println("isEmpty");
-//        IntSet instance = null;
-//        boolean expResult = false;
-//        boolean result = instance.isEmpty();
-//        assertEquals(expResult, result);
-//        // TODO review the generated test code and remove the default call to fail.
-//        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of has method, of class IntSet.
-     */
-    @Test
-    public void testHas() {
-        System.out.println("has");
-        boolean expResult;
-        boolean result;
-
-        expResult = false;
-        result = instance.has(TEST_VALUE);
-        assertEquals(expResult, result);
-
-//      expResult = false;
-        result = instance.has(TEST_VALUE + 1);
-        assertEquals(expResult, result);
-
-        instance.add(TEST_VALUE);
-
-        expResult = true;
-        result = instance.has(TEST_VALUE);
-        assertEquals(expResult, result);
-
-        expResult = false;
-        result = instance.has(TEST_VALUE + 1);
-        assertEquals(expResult, result);
-
-        instance.add(TEST_VALUE + 1);
-
-        expResult = true;
-        result = instance.has(TEST_VALUE);
-        assertEquals(expResult, result);
-
-//      expResult = true;
-        result = instance.has(TEST_VALUE + 1);
-        assertEquals(expResult, result);
-    }
-
-    /**
-     * Test of add method, of class IntSet.
-     */
-    @Test
-    public void testAdd() {
-        System.out.println("add");
-        assert instance.getCount() < instance.getCapacity();
-        int testValue;
-        boolean testHad;
-        int preCount;
-
-        testValue = TEST_VALUE;
-        testHad = instance.has(testValue);
-        assertEquals(false, testHad);
-        preCount = instance.getCount();
-        instance.add(testValue);
-        assertTrue(instance.has(testValue));
-        assertEquals(preCount + 1, instance.getCount());
-
-        testHad = instance.has(testValue);
-        assertEquals(true, testHad);
-        preCount = instance.getCount();
-        instance.add(testValue);
-        assertTrue(instance.has(testValue));
-        assertEquals(preCount, instance.getCount());
-
-        testValue = 943;
-
-        testHad = instance.has(testValue);
-        assertEquals(false, testHad);
-        preCount = instance.getCount();
-        instance.add(testValue);
-        assertTrue(instance.has(testValue));
-        assertEquals(preCount + 1, instance.getCount());
-
-        testValue = 28365;
-
-        testHad = instance.has(testValue);
-        assertEquals(false, testHad);
-        preCount = instance.getCount();
-        instance.add(testValue);
-        assertTrue(instance.has(testValue));
-        assertEquals(preCount + 1, instance.getCount());
-
-        testHad = instance.has(testValue);
-        assertEquals(true, testHad);
-        preCount = instance.getCount();
-        instance.add(testValue);
-        assertTrue(instance.has(testValue));
-        assertEquals(preCount, instance.getCount());
-    }
-
-    /**
-     * Test of remove method, of class IntSet.
-     */
-    @Test
-    public void testRemove() {
-        System.out.println("remove");
-        int value = 0;
-        instance = new IntSet(TEST_CAPACITY);
-        instance.remove(value);
-    }
-
-    /**
-     * Test of intersect method, of class IntSet.
-     */
-    @Test
-    public void testIntersect() {
-//        System.out.println("intersect");
-//        IntSet other = null;
-//        IntSet instance = null;
-//        IntSet expResult = null;
-//        IntSet result = instance.intersect(other);
-//        assertEquals(expResult, result);
-//        // TODO review the generated test code and remove the default call to fail.
-//        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of union method, of class IntSet.
-     */
-    @Test
-    public void testUnion() {
-//        System.out.println("union");
-//        IntSet other = null;
-//        IntSet instance = null;
-//        IntSet expResult = null;
-//        IntSet result = instance.union(other);
-//        assertEquals(expResult, result);
-//        // TODO review the generated test code and remove the default call to fail.
-//        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of getArray method, of class IntSet.
-     */
-    @Test
-    public void testGetArray() {
-//        System.out.println("getArray");
-//        IntSet instance = null;
-//        int[] expResult = null;
-//        int[] result = instance.getArray();
-//        assertArrayEquals(expResult, result);
-//        // TODO review the generated test code and remove the default call to fail.
-//        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of getCount method, of class IntSet.
-     */
     @Test
     public void testGetCount() {
-        System.out.println("getCount");
-        int expResult;
-        int result;
-        
-        expResult = 0;
-        result = instance.getCount();
-        assertEquals(expResult, result);
-        
-        instance.add(TEST_VALUE);
-        
-        expResult = 1;
-        result = instance.getCount();
-        assertEquals(expResult, result);
-        
-        instance.add(TEST_VALUE);
-        
-        expResult = 1;
-        result = instance.getCount();
-        assertEquals(expResult, result);
-        
-        instance.add(TEST_VALUE+1);
-        
-        expResult = 2;
-        result = instance.getCount();
-        assertEquals(expResult, result);
-        
-        instance.add(TEST_VALUE+1);
-        
-        expResult = 2;
-        result = instance.getCount();
-        assertEquals(expResult, result);
+        System.out.println("getCount()");
+        assertEquals(0, intSet.getCount());
     }
 
-    /**
-     * Test of getCapacity method, of class IntSet.
-     */
     @Test
-    public void testGetCapacity() {
-        System.out.println("getCapacity");
-        int expResult = TEST_CAPACITY;
-        int result = instance.getCapacity();
-        assertEquals(expResult, result);
+    public void TestGetCapacity() {
+        System.out.println("GetCapacity()");
+        assertEquals(CAPASITY, intSet.getCapacity());
     }
 
-    /**
-     * Test of toString method, of class IntSet.
-     */
     @Test
-    public void testToString() {
-//        System.out.println("toString");
-//        IntSet instance = null;
-//        String expResult = "";
-//        String result = instance.toString();
-//        assertEquals(expResult, result);
-//        // TODO review the generated test code and remove the default call to fail.
-//        fail("The test case is a prototype.");
+    public void testIntSet() {
+        System.out.println("IntSet(int capacity)");
+        assertEquals(0, intSet.getCount());
+        assertEquals(CAPASITY, intSet.getCapacity());
+    }
+
+    @Test
+    public void testHas() {
+        System.out.println("has(int value)");
+
+        assertTrue(!intSet.has(234));
+        assertTrue(!intSet.has(0));
+        
+        intSet.add(45);
+
+        assertTrue(intSet.has(45));
+    }
+
+    @Test
+    public void testAdd() {
+        System.out.println("add(int value)");
+        
+        
+        // set = []; count = 0;
+        // voeg een element toe
+        
+
+        intSet.add(254);
+        assertTrue(intSet.has(254));
+        assertEquals(1, intSet.getCount());
+        
+        // set = [254]; count = 1;
+        // probeer een bestaand element toe te voegen
+        intSet.add(254);
+        assertEquals(1, intSet.getCount());
+        assertTrue(intSet.has(254));
+        
+        // set = [254]; count = 1;
+        // kijk of toevoegen meerdere elementen werkt
+        intSet.add(25);
+        assertEquals(2, intSet.getCount());
+        assertTrue(intSet.has(25));
+        
+        // set = [254, 25]; count = 2;
+        intSet.add(315);
+        assertEquals(3, intSet.getCount());
+        assertTrue(intSet.has(315));
+        
+        // set = [254, 25, 315]; count = 3;
+        intSet.add(32);
+        assertEquals(4, intSet.getCount());
+        assertTrue(intSet.has(32));
+        
+        // set = [254, 25, 315, 32]; count = 4;
+        // test of negative getallen en nul het doen
+        intSet.add(-23);
+        assertEquals(5, intSet.getCount());
+        assertTrue(intSet.has(-23));
+        
+        // set = [254, 25, 315, 32, -23]; count = 5;
+        intSet.add(0);
+        assertEquals(6, intSet.getCount());
+        assertTrue(intSet.has(0));
+        
+        // set = [254, 25, 315, 32, 0]; count = 6;
+    }
+
+    @Test
+    public void testRemove() {
+        System.out.println("remove(int value)");
+
+        // set = []; count = 0;
+        // verwijder element van een lege array
+        intSet.remove(24);
+
+        assertTrue(!intSet.has(24));
+        assertEquals(0, intSet.getCount());
+
+        intSet.add(12);
+        intSet.add(24);
+        intSet.add(37);
+        intSet.add(23);
+
+        // set = [12, 24, 37, 23]; count = 4;
+        // verwijder bestaand element
+        intSet.remove(24);
+
+        assertEquals(3, intSet.getCount());
+        assertTrue( intSet.has(12));
+        assertTrue(!intSet.has(24));
+        assertTrue( intSet.has(37));
+        assertTrue( intSet.has(23));
+
+        // set = [12, 37, 23]; count = 3;
+        // verwijder twee keer een bestaand element
+
+        intSet.remove(12);
+        intSet.remove(12);
+
+        assertEquals(2, intSet.getCount());
+        assertTrue(!intSet.has(12));
+        assertTrue( intSet.has(37));
+        assertTrue( intSet.has(23));
+        // set = [37, 23]; count = 2;
+    }
+    
+    @Test
+    public void testIsEmpty(){
+        System.out.println("isEmpty()");
+        
+        // set = []; count = 0;
+        // kijken of de lijst leeg is naar initalisatie
+        assertEquals(true, intSet.isEmpty());
+        
+        intSet.add(25);
+        // set = [25]; count = 1;
+        // kijken of de set niet meer leeg is na toevoegen van een element
+        assertEquals(false, intSet.isEmpty());
+        
+        intSet.remove(25);
+        // set = []; count = 0;
+        // kijken of de set nleeg is na verwijderen van een element
+        assertEquals(true, intSet.isEmpty());
+        
+        intSet.add(25);
+        intSet.add(54);
+        intSet.add(27);
+        intSet.add(12);
+        intSet.add(12); // dubble check
+        intSet.remove(25); 
+        intSet.remove(25); // dubble check
+        intSet.remove(12);
+        intSet.remove(54);
+        intSet.remove(27);
+        // set = []; count = 0;
+        // kijken of de set nleeg is na toevoegen en verwijderen van meerdere elementen
+        assertEquals(true, intSet.isEmpty());
+    }
+    
+    @Test
+    public void testGetArray(){
+        System.out.println("getArray()");
+        
+        // set = [], count = 0;
+        // kijk of een lege lijst 0 lengte heeft
+        assertEquals(0, intSet.getArray().length);
+        
+        intSet.add(24);
+        intSet.add(34);
+        intSet.add(41);
+        // set = [24, 34, 41]; count = 3;
+        // kijk of na 3 element hij lengte 3 heeft
+        assertEquals(3, intSet.getArray().length);
+        
+        // kijk of echt alle elementen er nog insitten en de oorspronkelijke lijst niet is gewijziged
+        for (int i = 0; i < intSet.getArray().length; i++) {
+            assertTrue(intSet.has(intSet.getArray()[i]));
+        }
+    }
+    
+    @Test
+    public void testIntersect(){
+        System.out.println("intersect(IntSet other)");
+        IntSet intersectList;
+        IntSet otherIntSet = new IntSet(5);
+                
+        otherIntSet.add(25);
+        otherIntSet.add(37);
+        otherIntSet.add(58);
+        intSet.add(25);
+        intSet.add(28);
+        intSet.add(58);
+        intSet.add(23);
+        //      intSet: set = [25, 28, 58, 23], count = 4;
+        // otherIntSet: set = [25, 37, 58],     count = 3;
+        intersectList = intSet.intersect(otherIntSet);
+        assertEquals(2, intersectList.getCount());
+        assertTrue(intersectList.has(25));
+        assertTrue(intersectList.has(58));
+        for (int i = 0; i < intersectList.getCount(); i++) {
+             assertTrue(intSet.has(intersectList.getArray()[i]));
+             assertTrue(otherIntSet.has(intersectList.getArray()[i]));
+        }
+        
+        otherIntSet = new IntSet(5);
+        //      intSet: set = [25, 28, 58, 23], count = 4;
+        // otherIntSet: set = [],               count = 0;
+        // kijk of intersect het kan hebben als other leeg is
+        intersectList = intSet.intersect(otherIntSet);
+        assertEquals(0, intersectList.getCount());
+        
+        intSet = new IntSet(CAPASITY);
+        otherIntSet.add(25);
+        otherIntSet.add(37);
+        otherIntSet.add(58);
+        //      intSet: set = [],               count = 0;
+        // otherIntSet: set = [25, 37, 58],     count = 3;
+        // kijk of intersect het kan hebben als intSet zelf leeg is
+        intersectList = intSet.intersect(otherIntSet);
+        assertEquals(0, intersectList.getCount());
     }
 }
